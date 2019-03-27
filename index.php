0b895b34de79ff663e3b3452dde396131ce312ed
Daniel Lerner <daniellerner@Daniels-MacBook-Pro-2.local>
2019-03-27 09:08:18 -0400

    original commit for asgn09

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+require 'functions.php';
+
+class Task {
+  
+  public $description;
+  
+  public $completed = false;
+  
+  public function __construct($description)
+  {
+   $this->description = $description; 
+  }
+  
+  public function complete()
+  {
+   $this->completed = true;
+  }
+  
+  public function isComplete()
+  {
+   return $this->completed;
+  }
+}
+
+$tasks = [
+  new Task('Go to the store'),
+  new Task('Buy food'),
+  new Task('Buy liquor')
+  ];
+
+
+$tasks[0]->complete();
+
+require 'index.view.php';
